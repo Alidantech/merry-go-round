a09ac8321cb7e479b624866f43cea386859faf14
IsoDevMate <oumaoduor5827@gmail.com>
2024-03-27 16:37:45 +0300

    change to type module

---
+++ database/models/payment.js
@@ -0,0 +1,30 @@
+
+
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const lipaNaMpesaTransactionSchema = new Schema({
+    Amount: {
+        type: String,
+        required: true,
+    },
+    MpesaReceiptNumber: {
+        type: String,
+        required: true,
+    },
+    TransactionDate: {
+        type: String,
+        required: true,
+    },
+    PhoneNumber: {
+        type: String,
+        required: true,
+    },
+},
+    {
+        timestamps: true
+    });
+
+module.exports = mongoose.model('LipaNaMpesaTransaction', lipaNaMpesaTransactionSchema)
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 import express from "express";
 import cors from "cors";
 import { sendAlertViaSMS } from "./messages/sms.js";
-
+const transactions=require('./routes/payments.routes.js')
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const {connectDb} = require("./db.js");
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+connectDb()
+app.use('/api',transactions)
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
